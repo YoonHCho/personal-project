4f13fc76f33dd08a1d153c6b5860ed3991c036d7
Yoon Hwan Cho <yhcho0714@gmail.com>
2022-11-30 10:17:14 -0800

    functional buttons completed

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -16,76 +16,79 @@ const Hangman = () => {
     drawHangman(canvasRef, wrong);
   }, [wrong]);
 
-  useEffect(() => {
-    console.log('what');
-  }, [correctLetters, wrongLetters])
-
   const clickedButton = e => {
-    const selectedLetter = e.target.innerText;
-    // if the clicked letter is included in the word
-    if (wordArray.includes(selectedLetter)) {
-      // check if the clicked letter is included in the correctLetters array
-      // if it is not in the correctLetters array, push the letter to the array
-      if (!correctLetters.includes(selectedLetter)) {
-        setCorrectLetters((prev) => {
-          const toSet = prev;
-          toSet.push(selectedLetter);
-          return toSet;
-        });
-      }
-      setWrong(prev => prev + 0);
-    // if the clicked letter is NOT included in the word
-    } else {
-      // check if the clicked letter is included in the wrongLetters array
-      // if it is not included, push the letter to the wrongLetters array
-      if (!(wrongLetters.includes(selectedLetter))) {
-        setWrongLetters((prev) => {
-          const toSet = prev;
-          toSet.push(selectedLetter);
-          return toSet;
-        });
-
-        if (wrong >= 6) {
-          setWrong(prev => prev + 1);
-          return console.log('END OF GAME!')
+    if(e.target.nodeName === 'BUTTON') {
+      const selectedLetter = e.target.innerText;
+      // if the clicked letter is included in the word
+      if (wordArray.includes(selectedLetter)) {
+        // check if the clicked letter is included in the correctLetters array
+        // if it is not in the correctLetters array, push the letter to the array
+        if (!correctLetters.includes(selectedLetter)) {
+          setCorrectLetters(prev => {
+            const toSet = prev.concat(selectedLetter);
+            return toSet;
+          })
         }
+        setWrong(prev => prev + 0);
+      // if the clicked letter is NOT included in the word
+      } else {
+        // check if the clicked letter is included in the wrongLetters array
+        // if it is not included, push the letter to the wrongLetters array
+        if (!(wrongLetters.includes(selectedLetter))) {
+          setWrongLetters(prev => {
+            const toSet = prev.concat(selectedLetter);
+            return toSet;
+          });
 
-        setWrong(prev => prev + 1);
+          if (wrong >= 6) {
+            setWrong(prev => prev + 1);
+            return console.log('END OF GAME!')
+          }
+
+          setWrong(prev => prev + 1);
+        }
       }
+
     }
   }
 
   return (
     <div className="container font-monospace">
-      {console.log('hey')}
+      {console.log('RENDERED')}
       <div className='row justify-content-md-center'>
         <div className='col-md-6 row justify-content-md-center'>
           <canvas ref={canvasRef} />
         </div>
-        <div className='col-md-6 bg-dark text-white'>
-          <h1 className='text-center'>
+        <div className='col-md-6 align-items-center flex-nowrap'>
+          <h1 className='text-center my-3'>
           {
-            [...testWord].map(letter => {
+            wordArray.map(letter => {
               if (letter === ' ') {
                 return (
                   <br />
                 );
               } else {
-                return '_ ';
+                if (correctLetters.includes(letter)) {
+                  return ` ${letter}`;
+                } else {
+                  return ' _';
+                }
               }
             })
           }
           </h1>
-          <div className='text-center'>
+          <div className='text-center my-3'
+            onClick={clickedButton}
+          >
             {
               alphabet.map(each => {
                 if (each === 'P' || each === 'L') {
                   return (
                     <>
-                      <button onClick={clickedButton} key={each}
+                      <button key={each}
                         className={
                           correctLetters.includes(each) ? 'bg-success fs-1 mx-1 my-1'
-                          : (wrongLetters.includes(each) ? 'bg-danger fs-1 mx-1 my-1' : 'fs-1 mx-1 my-1')
+                          : (wrongLetters.includes(each) ? 'bg-danger fs-1 mx-1 my-1' : 'bg-secondary fs-1 mx-1 my-1')
                         }
                       >{each}</button>
                       <br />
@@ -94,10 +97,10 @@ const Hangman = () => {
                 }
                 return (
                   <>
-                    <button onClick={clickedButton} key={each}
+                    <button key={each}
                       className={
                             correctLetters.includes(each) ? 'bg-success fs-1 mx-1 my-1'
-                            : (wrongLetters.includes(each) ? 'bg-danger fs-1 mx-1 my-1' : 'fs-1 mx-1 my-1')
+                            : (wrongLetters.includes(each) ? 'bg-danger fs-1 mx-1 my-1' : 'bg-secondary fs-1 mx-1 my-1')
                           }
                     >{each}</button>
                   </>
--- src/container/draw-letter.js
+++ src/container/draw-letter.js
@@ -1,4 +1,6 @@
-export const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ];
+export const alphabet = [
+  'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'
+  ];
 
 export const drawHangman = (canvasRef, num) => {
   const canvas = canvasRef.current;
