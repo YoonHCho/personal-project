ce13de5031b03aa690a387fde3a71819cb7210fc
Yoon Hwan Cho <yhcho0714@gmail.com>
2023-01-16 00:12:21 -0800

    still need to resolve front end sign in issues

--- src/App.js
+++ src/App.js
@@ -2,18 +2,46 @@ import Navbar from "./components/navbar/Navbar";
 import Auth from "./components/auth/Auth";
 import { useState } from "react";
 import AppContext from "./lib/app-context";
+// import { parseRoute } from "./lib/parse-route";
+// import { PageContainer } from "./components/auth/page-container";
 
 function App() {
-  const [user, setUser] = useState(false);
+  const [currentUser, setCurrentUser] = useState(false);
+  // const [path, setPath] = useState("");
 
-  // const getUser = () => {
+  // useEffect(() => {
+  //   window.addEventListener("hashchange", () => {
+  //     setPath(parseRoute(window.location.hash));
+  //   });
+  //   console.log("path: ", path);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-  // }
+  const onSignIn = (response) => {
+    const { user, token } = response;
+    console.log("user.username: ", user.username);
+    console.log("token: ", token);
+    window.localStorage.setItem("react-context-jwt", token);
+    setCurrentUser(user.username);
+    // window.location.hash = "home";
+  };
+
+  // const renderPage = () => {
+  //   if (path === "home") {
+  //     return <Navbar />;
+  //   }
+  //   if (path === "register" || path === "sign-in" || path === "") {
+  //     return <Auth />;
+  //   }
+  // };
 
   return (
     <>
-      <AppContext.Provider value={setUser}>
-        <div style={{ marginTop: "50px" }}>{!user ? <Auth /> : <Navbar />}</div>
+      <AppContext.Provider value={{ currentUser, setCurrentUser, onSignIn }}>
+        <div style={{ marginTop: "50px" }}>
+          {/* <PageContainer>{renderPage()}</PageContainer> */}
+          {!currentUser ? <Auth /> : <Navbar />}
+        </div>
       </AppContext.Provider>
     </>
   );
---
+++ src/components/auth/page-container.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PageContainer = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
