2c43e3ed08e4a3d8c28e82735523d11b6dd3e6a6
Yoon Hwan Cho <yhcho0714@gmail.com>
2023-01-20 20:23:00 -0800

    trying baseurl

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:3000",
-  baseURL: "https://containers-us-west-177.railway.app/",
+  // baseURL: "https://containers-us-west-177.railway.app/api",    try this next
+  baseURL: "https://personal-project-production-cc4d.up.railway.app/api",
 });
